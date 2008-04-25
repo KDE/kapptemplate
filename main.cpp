93e99cc0dc4d4d61dbaf6f97a9cfba6762d419ad
Beat Wolf <asraniel@fryx.ch>
2008-04-25 13:08:30 +0000

    -added copyright to the main file
    svn path=/trunk/playground/devtools/kapptemplate/; revision=801008

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright  2008 by Anne-Marie Mahfouf <annma@kde.org>                 *
+ *   Copyright  2008 by Beat Wolf          <asraniel@fryx.ch>              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
