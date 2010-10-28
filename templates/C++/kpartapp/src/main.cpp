c425ec4d9e405c187ff6bf19563a8f5ebc51c2b2
Anne-Marie Mahfouf <annma@kde.org>
2010-10-28 12:42:43 +0000

    add license header and proper year
    svn path=/trunk/KDE/kdesdk/kapptemplate/; revision=1190644

--- templates/C++/kpartapp/src/main.cpp
+++ templates/C++/kpartapp/src/main.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
 #include "%{APPNAMELC}.h"
 #include <kapplication.h>
 #include <kaboutdata.h>
@@ -11,7 +30,7 @@ static const char version[] = "%{VERSION}";
 
 int main(int argc, char **argv)
 {
-    KAboutData about("%{APPNAMELC}", 0, ki18n("%{APPNAME}"), version, ki18n(description), KAboutData::License_GPL, ki18n("(C) 2008 %{AUTHOR}"), KLocalizedString(), 0, "%{EMAIL}");
+    KAboutData about("%{APPNAMELC}", 0, ki18n("%{APPNAME}"), version, ki18n(description), KAboutData::License_GPL, ki18n("(C) %{CURRENT_YEAR} %{AUTHOR}"), KLocalizedString(), 0, "%{EMAIL}");
     about.addAuthor( ki18n("%{AUTHOR}"), KLocalizedString(), "%{EMAIL}" );
     KCmdLineArgs::init(argc, argv, &about);
 
