3d9a8e2f8285b695c0a55dae0e52cd90dbe91d07
Anne-Marie Mahfouf <annma@kde.org>
2010-10-28 12:20:30 +0000

    improve plasmoid template: new preview, add license headers, improve applet layout
    svn path=/trunk/KDE/kdesdk/kapptemplate/; revision=1190640

--- templates/C++/plasmoid/%{APPNAMELC}.cpp
+++ templates/C++/plasmoid/%{APPNAMELC}.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
 #include "%{APPNAMELC}.h"
 #include <QPainter>
 #include <QFontMetrics>
@@ -48,10 +67,11 @@ void %{APPNAME}::paintInterface(QPainter *p,
     m_svg.resize((int)contentsRect.width(), (int)contentsRect.height());
     m_svg.paint(p, (int)contentsRect.left(), (int)contentsRect.top());
 
-    // We place the icon and text
-    p->drawPixmap(7, 0, m_icon.pixmap((int)contentsRect.width(),(int)contentsRect.width()-14));
+    // We place the icon and text on our background
+    p->drawPixmap(25, 40, m_icon.pixmap((int)contentsRect.width(),(int)contentsRect.width()-50));
     p->save();
     p->setPen(Qt::white);
+    p->setFont(QFont("Times", 15, QFont::Bold));
     p->drawText(contentsRect,
                 Qt::AlignBottom | Qt::AlignHCenter,
                 i18n("Hello Plasmoid!"));
--- templates/C++/plasmoid/%{APPNAMELC}.h
+++ templates/C++/plasmoid/%{APPNAMELC}.h
@@ -1,9 +1,28 @@
+/***************************************************************************
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
 // Here we avoid loading the header multiple times
 #ifndef %{APPNAMEUC}_HEADER
 #define %{APPNAMEUC}_HEADER
-// We need the Plasma Applet headers
-#include <KIcon>
 
+#include <KIcon>
+// We need the Plasma Applet headers
 #include <Plasma/Applet>
 #include <Plasma/Svg>
 
@@ -18,7 +37,7 @@ class %{APPNAME} : public Plasma::Applet
         %{APPNAME}(QObject *parent, const QVariantList &args);
         ~%{APPNAME}();
 
-        // The paintInterface procedure paints the applet to screen
+        // The paintInterface procedure paints the applet to the desktop
         void paintInterface(QPainter *painter,
                 const QStyleOptionGraphicsItem *option,
                 const QRect& contentsRect);
