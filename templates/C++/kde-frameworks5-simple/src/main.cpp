917d162f5e785c2a396ccff5aaa9c83230b89194
Jonathan Riddell <jr@jriddell.org>
2014-07-28 16:34:52 +0200

    Add KDE Frameworks 5 template

---
+++ templates/C++/kde-frameworks5-simple/src/main.cpp
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+// application header
+#include "%{APPNAMELC}.h"
+// KDE headers
+#include <QApplication>
+#include <QCommandLineParser>
+#include <KAboutData>
+#include <KLocalizedString>
+
+int main(int argc, char **argv)
+{
+    QApplication application(argc, argv);
+
+    KLocalizedString::setApplicationDomain("%{APPNAMELC}");
+    KAboutData aboutData( QStringLiteral("%{APPNAMELC}"),
+                          i18n("Simple App"),
+                          QStringLiteral("0.1"),
+                          i18n("A Simple Application written with KDE Frameworks"),
+                          KAboutLicense::GPL,
+                          i18n("(c) 20013-2014, %{AUTHOR} <%{EMAIL}>"));
+
+    aboutData.addAuthor(i18n("%{AUTHOR}"),i18n("Author"), QStringLiteral("%{EMAIL}"));
+    aboutData.setProgramIconName("%{APPNAMELC}");
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    aboutData.setupCommandLine(&parser);
+    parser.process(application);
+    aboutData.processCommandLine(&parser);
+
+    %{APPNAME} *appwindow = new %{APPNAME};
+    appwindow->show();
+    return application.exec();
+}
