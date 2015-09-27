06b4acec142f0c2993a671f3ad3c5e095a72e999
Simon Wächter <waechter.simon@gmail.com>
2015-09-27 20:00:33 +0200

    Remove old templates

--- src/templates/C++/flake/%{APPNAME}Widget.cpp
+++
@@ -1,8 +0,0 @@
-#include "%{APPNAME}Widget.h"
-#include "%{APPNAME}Shape.h"
-
-#include <KDebug>
-
-%{APPNAME}Widget::%{APPNAME}Widget(%{APPNAME}Shape *shape, QWidget *parent) : QWidget(parent), m_shape(shape)
-{
-}
